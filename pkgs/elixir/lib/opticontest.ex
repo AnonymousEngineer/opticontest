9b77d04f43d50a5e24abcd26bab155c3db9a9eab
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-09-14 23:56:57 +0000

    Add Elixir package skeleton

---
+++ pkgs/elixir/lib/opticontest.ex
@@ -0,0 +1,5 @@
+defmodule Opticontest do
+  def hello do
+    :world
+  end
+end
---
+++ pkgs/elixir/test/opticontest_test.exs
@@ -0,0 +1,8 @@
+defmodule Opticontest.Test do
+  use ExUnit.Case
+  doctest Opticontest
+
+  test "greets the world" do
+    assert Opticontest.hello() == :world
+  end
+end
